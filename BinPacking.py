1ccc0ac2e10c6210476d8a8a1f2f57ac019cba96
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2016-01-15 18:13:14 -0700

    Create BinPacking.py

---
+++ BinPacking.py
@@ -0,0 +1,19 @@
+import Driver
+
+"""
+FIND_SOLUTION:
+    Define this function in BinPacking.py, along with any auxiliary
+functions that you need.  Do not change the Driver.py file at all.
+--------------------------------------------------
+rectangles: a list of tuples, e.g. [(w1, l1), ... (wn, ln)] where
+    w1 = width of rectangle 1,
+    l1 = length of rectangle 1, etc.
+--------------------------------------------------
+RETURNS: a list of tuples that designate the top left corner placement,
+         e.g. [(x1, y1), ... (xn, yn)] where
+         x1 = top left x coordinate of rectangle 1 placement
+         y1 = top left y coordinate of rectangle 1 placement, etc.
+"""
+
+def find_solution(rectangles):
+    return Driver.find_naive_solution(rectangles)  # a working example!
